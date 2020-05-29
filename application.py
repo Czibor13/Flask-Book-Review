96e077cdbf202ac82ec623047f9c80e6bc39e381
unknown <czibor13@yahoo.com>
2020-05-29 10:07:23 -0500

    Added logging in and out

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -34,5 +34,40 @@ def register():
         db.execute("INSERT INTO users (username, password) VALUES (:username, :password)", {"username": username, "password": password})
         db.commit()
         return render_template("register.html", success=True)
+    # Returns a page for users to register, or redirects to the index if logged in
     else: #if request.method == "GET":
+        if "user" in session:
+            # Already Logged in
+            return redirect(url_for("index"))
+        # Not logged in currently
         return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # Attempts to log in the user based on the information given in the login form
+    if request.method =="POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # Check if there is a matching username and password combo
+        if db.execute("SELECT username FROM users WHERE username = :username AND password = :password", {"username": username, "password": password}).rowcount == 1:
+            # Log user in with the username
+            session["user"] = username
+            return render_template("login.html", message="You have logged in", success=True)
+        else:
+            # The information given was not a match
+            return render_template("login.html", message="Invalid Credientials", failed=True)
+
+    # Returns a page for users to log in, or redirects to the index if logged in
+    else: #if request.method == "GET":
+        if "user" in session:
+            # Already Logged in
+            return redirect(url_for("index"))
+        # Not logged in currently
+        return render_template("login.html")
+
+# Logs the user out
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
