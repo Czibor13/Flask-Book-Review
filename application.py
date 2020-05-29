a840682682294094f2ec3ad685a63a203d09e6df
unknown <czibor13@yahoo.com>
2020-05-29 09:27:47 -0500

    Added layout file and an index page.

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, render_template, session
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -20,7 +20,7 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-
+# Returns Homepage
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
